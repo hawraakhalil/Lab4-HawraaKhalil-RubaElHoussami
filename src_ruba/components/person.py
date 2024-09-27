9e263618575df292da93e91f5fea879297dd4713
hawraakhalil <hhk22@mail.aub.edu>
2024-09-27 13:58:00 +0300

    deleting files

--- src_ruba/components/person.py
+++
@@ -1,8 +0,0 @@
-class Person:
-    def __init__(self, name: str, age: int, email: str):
-        self.name = name
-        self.age = age
-        self._email = email
-    
-    def introduce(self):
-        print(f'Hello, my name is {self.name} and I am {self.age} years old.')
